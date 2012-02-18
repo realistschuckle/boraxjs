b1bede8daa4fd2dd7e27f6979bdabaef57e7797c
Curtis Schlak <fuzzygeek1@users.sourceforge.net>
2012-02-18 06:01:24 -0600

    Not a good test file.

--- test/client/default-authentication-handler-tests.js
+++
@@ -1,9 +0,0 @@
-var vows = require('vows')
-  , assert = require('assert')
-  , boric = require('../../lib/borax-in-client')
-  , ajax = require('./fakejax')
-  ;
-
-vows.describe('default authentication handler').addBatch({
-  
-}).export(module);
